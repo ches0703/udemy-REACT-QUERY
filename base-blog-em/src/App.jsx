d15a08b68d2a8e63b325f6bd82945ede6a5ee012
ches0703 <ches0703@naver.com>
2024-09-08 22:16:20 +0900

    section 1 : QueryClinent & QueryClientProvider 추가

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,13 +1,18 @@
 import { Posts } from "./Posts";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryCline = new QueryClient();
 
 function App() {
   return (
     // provide React Query client to App
-    <div className="App">
-      <h1>Blog &apos;em Ipsum</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={queryCline}>
+      <div className="App">
+        <h1>Blog &apos;em Ipsum</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
   );
 }
 
